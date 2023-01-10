5a652f1df956a65d2ee89af13bbd1a28739faa47
Sejin-999 <76008226+Sejin-999@users.noreply.github.com>
2023-01-10 17:52:57 +0900

    helloApp

---
+++ helloApp/heyAppleApp/test.py
@@ -0,0 +1,57 @@
+import smtplib
+import json
+import requests
+from parse import *
+from email.mime.text import MIMEText
+
+#exParam = requests.get("http://127.0.0.1:8000", params={"email" : "1106q@naver.com" , "orderbillid" : "1"})
+#print(exParam.text)
+context = "TEST 입니다"
+
+# json setting Start
+exJson = '{"email" : "1106q@naver.com" , "orderbillid" : "1"}'
+
+#mail setting Start
+def appleMailSetting(exJson):
+    #setting start
+    global email , subject , orderbillid
+    loadsJson = json.loads(exJson)
+    email = loadsJson["email"]
+    orderbillid = loadsJson["orderbillid"]
+    #setting end
+    
+    # parshing start
+    idparse = parse("{}@naver.com",email)
+    subject = idparse[0]+"님 구매 감사합니다!"
+    # parshing end
+    
+    # Test code start
+    print(exJson)
+    print("제목 : "+subject + "\n이메일 : "+email +"\n오더 아이디 : "+orderbillid) 
+    # Test code end
+#mail setting End
+
+#json setting end
+
+#mail Send Start
+def appleMail(email , context):
+    smtp = smtplib.SMTP('smtp.gmail.com',587)
+    smtp.starttls()
+    smtp.login('testproject9197@gmail.com','joilxdqyvpihtlkf')
+
+
+    msg = MIMEText(context)
+    msg['Subject'] = subject
+
+    smtp.sendmail('testproject9197@gmail.com',email,msg.as_string())
+
+    smtp.quit()    
+#mail Send End
+# 함수실행 줄 Start
+appleMailSetting(exJson)
+appleMail(email ,context)
+# 함수실행 줄 End
+
+# check value Start
+#print(subject)
+# check value End
---
+++ helloApp/heyAppleApp/views.py
@@ -0,0 +1,40 @@
+import pymysql
+from django.http import JsonResponse
+import boto3
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Fruit
+from .serializers import TestFruitSerializer
+
+# Create your views here.
+
+#db = pymysql.Connect(host="localhost" , user="root" , password="1234" , database="heyAppledb")
+#cursor = db.cursor();
+
+# 전체 정보 넘겨주는 함수 ... 삭제되거나 , 값이 없는 경우 에외처리
+@api_view(['GET'])
+def getFruitInfo(request):
+    datas = Fruit.objects.filter(is_deleted =1).all()
+    if datas is not None:  # datas != none
+        serializer = TestFruitSerializer(datas , many=True)
+        return Response(serializer.data , status=status.HTTP_200_OK)
+    return JsonResponse({"msg":"Infomation not found"} , status=status.HTTP_401_UNAUTHORIZED)
+
+
+# 찾고 싶은 과일 정보 찾기 (세부페이지)
+@api_view(['POST'])
+def postFruitFind(request):
+    reqData = request.data
+    name = reqData['name']  #id or name 
+    data = Fruit.objects.filter(name = name)
+    if data is not None:  # datas != none
+        serializer = TestFruitSerializer(data,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return JsonResponse({"msg":"Infomation not found"} , status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
